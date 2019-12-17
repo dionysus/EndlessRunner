49f8be9a54326622d2c1b2c1c9798fb97e1be596
dionysus_pc <dionysus.cho@gmail.com>
2019-12-17 14:53:13 -0500

    Added Spawner script to generate Obstacles

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject[] prefabs;
+	public float delay = 2.0f;
+	public bool active = true;
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(EnemyGenerator());
+	}
+
+	IEnumerator EnemyGenerator(){
+		yield return new WaitForSeconds(delay);
+		if(active){
+			var newTransform = transform;
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], newTransform.position, Quaternion.identity);
+		}
+		StartCoroutine(EnemyGenerator());
+	}
+}
