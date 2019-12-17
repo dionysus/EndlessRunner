f9b3aac103a00b40346810ad9d10a3a1168d3a27
dionysus_pc <dionysus.cho@gmail.com>
2019-12-17 14:24:40 -0500

    Added FloorTexture material to foreground, and applied animate and tiling scripts.

--- Assets/Scripts/TiledBackground.cs
+++ Assets/Scripts/TiledBackground.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TiledBackground : MonoBehaviour
 {
 	public int textureSize = 32;
+	public bool scaleHorizontally = true;
+	public bool scaleVertically = true;
 	// Start is called before the first frame update
 	void Start()
 	{
-		var newWidth = Mathf.Ceil(Screen.width/(textureSize * PixelPerfectCamera.scale));
-		var newHeight = Mathf.Ceil(Screen.height/(textureSize * PixelPerfectCamera.scale));
+		var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil(Screen.width/(textureSize * PixelPerfectCamera.scale));
+		var newHeight = !scaleVertically ? 1 : Mathf.Ceil(Screen.height/(textureSize * PixelPerfectCamera.scale));
 		transform.localScale = new Vector3(newWidth*textureSize, newHeight*textureSize, 1);
 		GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
 	}
