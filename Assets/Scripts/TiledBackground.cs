24665611c188ab06d94e8b7c871f1a22b93667da
dionysus_pc <dionysus.cho@gmail.com>
2019-12-17 13:54:15 -0500

    Added TileBackground.cs to auto-fill background with WallTexture

---
+++ Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour
+{
+	public int textureSize = 32;
+	// Start is called before the first frame update
+	void Start()
+	{
+		var newWidth = Mathf.Ceil(Screen.width/(textureSize * PixelPerfectCamera.scale));
+		var newHeight = Mathf.Ceil(Screen.height/(textureSize * PixelPerfectCamera.scale));
+		transform.localScale = new Vector3(newWidth*textureSize, newHeight*textureSize, 1);
+		GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+	}
+}
