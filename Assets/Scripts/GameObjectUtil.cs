1cdef216edaa6768e0ba9a28991660910ec81300
dionysus_pc <dionysus.cho@gmail.com>
2019-12-17 16:08:29 -0500

    Added ObjectPool to collect and recycle offscreen gameObjects.

--- Assets/Scripts/GameObjectUtil.cs
+++ Assets/Scripts/GameObjectUtil.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameObjectUtil{
-
+	private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>();
 	public static GameObject Instantiate(GameObject prefab, Vector3 pos){
 		GameObject instance = null;
-		instance = GameObject.Instantiate (prefab);
-		instance.transform.position = pos;
 
+		var recycledScript = prefab.GetComponent<RecycleGameObject>();
+		if (recycledScript != null) {
+			var pool = GetObjectPool(recycledScript);
+			instance = pool.NextObject(pos).gameObject;
+		} else {
+			instance = GameObject.Instantiate (prefab);
+			instance.transform.position = pos;
+		}
 		return instance;
 	}
 
@@ -23,4 +29,16 @@ public class GameObjectUtil{
 		}
 	}
 
+	private static ObjectPool GetObjectPool(RecycleGameObject reference){
+		ObjectPool pool = null;
+		if(pools.ContainsKey(reference)){
+			pool = pools[reference];
+		} else {
+			var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
+			pool = poolContainer.AddComponent<ObjectPool>();
+			pool.prefab = reference;
+			pools.Add(reference, pool);
+		}
+		return pool;
+	}
 }
