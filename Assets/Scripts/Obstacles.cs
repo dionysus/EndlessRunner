c7a8581d07d07f83bba074d7c100e498fc8830ec
dionysus_pc <dionysus.cho@gmail.com>
2020-02-17 10:35:30 -0500

    created Obstacles class with sprite randomizer

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour, IRecycle
+{
+
+    public Sprite[] sprites;
+
+    public void Restart() {
+        var renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    public void ShutDown() {
+        // Nothing
+    }
+}
