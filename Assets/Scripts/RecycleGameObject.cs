ef814676eea4b711e8c362bb8c5c305b8fca33f5
dionysus_pc <dionysus.cho@gmail.com>
2020-02-17 10:16:04 -0500

    create IRecycle interface, loops to recycle/shutdown all

--- Assets/Scripts/RecycleGameObject.cs
+++ Assets/Scripts/RecycleGameObject.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RecycleGameObject : MonoBehaviour
-{
+public interface IRecycle {
+	void Restart();
+	void ShutDown();
+}
+
+public class RecycleGameObject : MonoBehaviour {
+
+	private List<IRecycle> recycleComponents;
+
+	void Awake(){
+		// Gets list of all components, tests if they have the IR interface,
+		// if so, adds it to the list as a recyclable component
+		var components = GetComponents<MonoBehaviour> ();
+		recycleComponents = new List<IRecycle> ();
+
+		foreach(var component in components) {
+			if (component is IRecycle) { // does component implement interface?
+				recycleComponents.Add (component as IRecycle);
+			}
+		}
+
+		Debug.Log(name + " Found " + recycleComponents.Count + " Components");
+
+	}
+
 	public void Restart(){
 		gameObject.SetActive(true);
+		foreach (var component in recycleComponents) {
+			component.Restart();
+		}
 	}
 
 	public void ShutDown(){
 		gameObject.SetActive(false);
+		foreach (var component in recycleComponents) {
+			component.ShutDown();
+		}
 	}
 }
